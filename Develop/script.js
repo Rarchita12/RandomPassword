0b2105f8dc418657dcc4ca89a2ae85d98650d6df
Archita Racherla <archita.racherla@gmail.com>
2021-04-09 15:37:19 -0500

    added logic to generate random password based on selected criteria

--- Develop/script.js
+++ Develop/script.js
@@ -6,8 +6,8 @@ var generateBtn = document.querySelector("#generate");
 
 var specialChars = ['\u0020','\u0021', '\u0022', '\u0023', '\u0024', '\u0025', '\u0026', '\u0027', '\u0028', '\u0029', '\u002A', '\u002B', '\u002C', '\u002D', '\u002E', '\u002F', '\u003A', '\u003B', '\u003C', '\u003D', '\u003E', '\u003F', '\u0040', '\u005B', '\u005C', '\u005D', '\u005E', '\u005F', '\u0060', '\u007B', '\u007C', '\u007D', '\u007E'];
 var numericalChars =['0','1','2','3','4','5','6','7','8','9'];
-var lowercaseChars = [];
-
+var lowercaseChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercaseChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 // Write password to the #password input
 function writePassword() {
@@ -17,16 +17,16 @@ function writePassword() {
    window.alert("Password must be at least 8 characters and no more than 128 characters.");
  }
  else{
-   var specialChars = window.confirm("Click OK to confirm including special characters.");
+   var specChars = window.confirm("Click OK to confirm including special characters.");
    var numChars = window.confirm("Click OK to confirm including numerical characters. ");
    var lowerChars = window.confirm("Click OK to confirm including lowercase characters. ");
    var upperChars = window.confirm("Click OK to confirm including uppercase characters. ");
-   
+  
 
    if(specialChars === false && numChars === false && lowerChars === false && upperChars ===false){
           
    }
-   var password = generatePassword();
+   var password = generatePassword(totalChars, specChars, numChars, lowerChars, upperChars);
  }
  
   var passwordText = document.querySelector("#password");
@@ -35,12 +35,75 @@ function writePassword() {
 
 }
 
-function generatePassword(totalChars, specialChars, numChars, lowerChars, upperChars){
-var password = " ";
-var selectionsArray = [];
-for(var i =0; i<totalChars; i++){
+function generatePassword(totalChars, specChars, numChars, lowerChars, upperChars){
+var pass = " ";
 
+var selectionsArray = [];
+
+//populate selectionsArray and ensure at least 1 of every selection made is in the password
+if(specChars === true){
+  selectionsArray.push(1);
+  pass = pass.concat(specialChars[Math.floor((Math.random() * (33-0)+0))]);
+  console.log(pass);
+  console.log(selectionsArray);
 }
+
+if(numChars === true){
+  selectionsArray.push(2);
+  pass = pass.concat(numericalChars[Math.floor((Math.random() * (10-0)+0))]);
+  console.log(pass);
+  console.log(selectionsArray);
+}
+
+
+if(lowerChars === true){
+  selectionsArray.push(3);
+  pass = pass.concat(lowercaseChars[Math.floor((Math.random() * (26-0)+0))]);
+  console.log(pass);
+  console.log(selectionsArray);
+}
+
+if(upperChars === true){
+  selectionsArray.push(4);
+  pass = pass.concat(uppercaseChars[Math.floor((Math.random() * (26-0)+0))]);
+  console.log(pass);
+  console.log(selectionsArray);
+}
+
+var max = selectionsArray.length;
+
+
+
+
+for(var i =0; i<(totalChars-selectionsArray.length); i++){
+     var select = selectionsArray[Math.floor((Math.random() * (max-0)+0))];
+     console.log(select);
+     switch(select){
+       case 1:{
+           pass = pass.concat(specialChars[Math.floor((Math.random() * (33-0)+0))]);
+           break;
+       }
+
+       case 2:
+         {
+          pass = pass.concat(numericalChars[Math.floor((Math.random() * (10-0)+0))]);
+          break;
+         }
+
+         case 3:
+           {
+            pass = pass.concat(lowercaseChars[Math.floor((Math.random() * (26-0)+0))]);
+            break;
+           }
+
+           case 4:{
+            pass = pass.concat(uppercaseChars[Math.floor((Math.random() * (26-0)+0))]);
+            break;
+           }
+     }
+}
+return pass;
+
 }
 
 
